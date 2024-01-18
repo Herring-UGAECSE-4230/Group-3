d19b9599acee6ff04948c151eced206afe06ed85
Group 3 Raspberry Pi <connor.loosemore2021@gmail.com>
2024-01-18 14:48:12 -0500

    Deleted test and added blink led with RPi.GPIO and wiringpi functionality

---
+++ blink_led.py
@@ -0,0 +1,36 @@
+#Variables
+freq = 1
+pin = 21
+
+# #RPi.GPIO method
+# import RPi.GPIO as RPI
+# from time import sleep
+# 
+# freq = 10000
+# 
+# #Setup GPIO
+# RPI.setwarnings(False)
+# RPI.setmode(RPI.BCM)
+# RPI.setup(pin, RPI.OUT, initial=RPI.LOW)
+# 
+# #Blink the LED
+# while True:
+#     RPI.output(pin, RPI.HIGH)
+#     sleep((1/freq)/2)
+#     RPI.output(pin, RPI.LOW)
+#     sleep((1/freq)/2)
+
+
+#WiringPi method
+import wiringpi as WPI
+
+WPI.wiringPiSetupGpio()
+WPI.softToneCreate(pin)
+WPI.softToneWrite(pin, freq)
+
+while True:
+    #do nothing
+    pass
+WPI.softToneWrite(pin, 0)
+
+
