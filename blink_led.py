bf5939fbbcec18729198482f03eb6be7d69bea72
Group 3 Raspberry Pi <connor.loosemore2021@gmail.com>
2024-01-25 14:36:07 -0500

    Added comments, completed pigpio method

--- blink_led.py
+++ blink_led.py
@@ -1,24 +1,23 @@
 #Variables
-freq = 1
+freq = 8000
 pin = 21
 
-# #RPi.GPIO method
-# import RPi.GPIO as RPI
-# from time import sleep
-# 
-# freq = 10000
-# 
-# #Setup GPIO
-# RPI.setwarnings(False)
-# RPI.setmode(RPI.BCM)
-# RPI.setup(pin, RPI.OUT, initial=RPI.LOW)
-# 
-# #Blink the LED
-# while True:
-#     RPI.output(pin, RPI.HIGH)
-#     sleep((1/freq)/2)
-#     RPI.output(pin, RPI.LOW)
-#     sleep((1/freq)/2)
+#RPi.GPIO method
+import RPi.GPIO as RPI
+from time import sleep
+
+#Setup GPIO
+RPI.setwarnings(False)
+RPI.setmode(RPI.BCM)
+RPI.setup(pin, RPI.OUT, initial=RPI.LOW)
+
+half_period = ((1/freq)/2) #calculating sleep times for high and low sections
+#Blink the LED
+while True:
+    RPI.output(pin, RPI.HIGH) #generating square wave
+    sleep(half_period)
+    RPI.output(pin, RPI.LOW)
+    sleep(half_period)
 
 
 #WiringPi method - WiringPi does not work, remember to document issues in report
@@ -34,6 +33,18 @@ pin = 21
 # WPI.softToneWrite(pin, 0)
 
 #pigpio method
+import pigpio
 
+#duty cycle
+duty = 127
 
+pi = pigpio.pi()
+#setting up frequency and duty cycle
+pi.set_PWM_frequency(pin, freq)
+pi.set_PWM_dutycycle(pin, duty)
 
+while True:
+    #do nothing
+    pass
+#turn off the square wave
+pi.set_PWM_dutycycle(pin, 0)
