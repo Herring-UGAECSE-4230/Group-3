b58b98bfcce30100884331f8ff576e1b1e65e7f2
jo0wi <jrw22710@uga.edu>
2024-02-08 16:33:26 -0500

    auto_clock to ssd Updated (not working)

--- clock_with_7sd/auto_clock.py
+++ clock_with_7sd/auto_clock.py
@@ -1,7 +1,13 @@
 from datetime import datetime
+from time import sleep
+import RPi.GPIO as rpi
+
 
 def auto_clock():
+
     while True:
+
+        sleep(.25)
         now=datetime.now()
         minute = now.minute
         hour = now.hour
@@ -9,9 +15,42 @@ def auto_clock():
         if hour > 12:
             hour = hour - 12
 
-        ssd_h = '{0:02d}'.format(hour)
-        ssd_m = '{0:02d}'.format(minute)
+        clock_string = ('{0:02d}'.format(hour) + '{0:02d}'.format(minute))
+        clock_list = [*clock_string]
 
-        print(ssd_h, ":", ssd_m)
         
+
+        for i, val in enumerate(clock_list):
+            clock = i + 1
+            digit = val
+
+            if clock == 1:
+                rpi.output(CLK1,rpi.HIGH)
+                sleep(1/sample_rate)
+                rpi.output(CLK1,rpi.LOW)
+                sleep(1/sample_rate)
+
+            elif clock == 2:
+                rpi.output(CLK2,rpi.HIGH)
+                sleep(1/sample_rate)
+                rpi.output(CLK2,rpi.LOW)
+                sleep(1/sample_rate)
+                rpi.output(pin_list.LOW)
+
+            elif clock == 3:
+                rpi.output(CLK3,rpi.HIGH)
+                sleep(1/sample_rate)
+                rpi.output(CLK3,rpi.LOW)
+                sleep(1/sample_rate)
+                rpi.output(pin_list.LOW)
+
+            elif clock == 4:
+                rpi.output(CLK4,rpi.HIGH)
+                sleep(1/sample_rate)
+                rpi.output(CLK4,rpi.LOW)
+                sleep(1/sample_rate)
+                rpi.output(pin_list.LOW)
+        
+            
+
 auto_clock()
