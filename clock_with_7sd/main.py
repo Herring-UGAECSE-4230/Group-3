e0dafd938ba53891912d847a658438b4e44944d2
Group 3 Pi <connor.loosemore2021@gmail.com>
2024-02-15 13:33:24 -0500

    Checkpoint 2 completed

--- clock_with_7sd/main.py
+++ clock_with_7sd/main.py
@@ -8,7 +8,13 @@ q = Queue()
 inputThread = Thread(target = keypad.readKeypad, args = (q, ))
 inputThread.start()
 
+prev_state = '1'
+output_on = True
+
 while True:
     input = q.get()
-    flip_flop(input)
-    print(input + "\n")
+    if (input == '#'):
+        output_on = not output_on
+    else:
+        prev_state = input
+    seven_segment_code.flip_flop(input, output_on, prev_state)
--- clock_with_7sd/seven_segment_code.py
+++ clock_with_7sd/seven_segment_code.py
@@ -5,7 +5,8 @@ from time import sleep
 
 pin_list = [4,27,25,12,13,5,6,26]
 
-rpi.setmode(pin_list, rpi.OUT)
+rpi.setmode(rpi.BCM)
+rpi.setup(pin_list, rpi.OUT)
 
 #declaring gpio outputs
 a = 4
@@ -17,6 +18,8 @@ f = 5
 g = 6
 DP = 26
 CLK = 16
+
+rpi.setup(CLK, rpi.OUT)
 
 #setting seven segment values
 one = [f,e]
@@ -30,80 +33,80 @@ eight = [a,b,c,d,e,f,g]
 nine = [a,b,c,d,f,g]
 zero = [a,b,c,d,e,f]
 A = [a,b,c,e,f,g]
-B = [a,b,c,d,e,f,g,DP]
+B = [c,d,e,f,g]
 C = [a,d,e,f]
-D = [a,b,c,d,e,f,DP]
+D = [b,c,d,e,g]
 
 
-def flip_flop(x):
-        
-        
-#sending values to flip flop
+def flip_flop(x, on, prev_state):
+        #sending values to flip flop
+        if x == "#":
+                if not on:
+                        rpi.output(pin_list, rpi.LOW)
+                else:
+                        x = prev_state
         if x == "1":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(one, rpi.HIGH)
                 
         elif x == "2":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(two, rpi.HIGH)
 
         elif x == "3":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(three, rpi.HIGH)
         
         elif x == "4":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(four, rpi.HIGH)
 
         elif x == "5":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(five, rpi.HIGH)
 
         elif x == "6":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(six, rpi.HIGH)
 
         elif x == "7":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(seven, rpi.HIGH)
 
         elif x == "8":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(eight, rpi.HIGH)
         
         elif x == "9":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(nine, rpi.HIGH)
 
         elif x == "0":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(zero, rpi.HIGH)
 
         elif x == "A":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(A, rpi.HIGH)
 
         elif x == "B":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(B, rpi.HIGH)
 
         elif x == "C":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(C, rpi.HIGH)
         
         elif x == "D":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(D, rpi.HIGH)
-        elif x == "#":
-                rpi.output(pin_list.LOW)
-                rpi.output(g, rpi.HIGH)
         
         elif x == "*":
-                rpi.output(pin_list.LOW)
+                rpi.output(pin_list, rpi.LOW)
                 rpi.output(DP, rpi.HIGH)
 
-#        rpi.output(CLK,rpi.HIGH)
-#        sleep(1/sample_rate)
-#        rpi.output(CLK,rpi.LOW)
-#        sleep(1/sample_rate)
-#        rpi.output(pin_list.LOW)
+        rpi.output(CLK,rpi.HIGH)
+        sleep(.01)
+        rpi.output(CLK,rpi.LOW)
+        sleep(.01)
+        rpi.output(pin_list, rpi.LOW)
