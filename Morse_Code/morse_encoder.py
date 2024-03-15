4f0d0ee54a909ff3d3d6789ac4a3100d32ed12d8
jo0wi <jrw22710@uga.edu>
2024-03-15 16:43:49 -0400

    morse_encoder working

--- Morse_Code/morse_encoder.py
+++ Morse_Code/morse_encoder.py
@@ -8,19 +8,30 @@ codes_dict = { 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.',
 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--',
 'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--',
 '4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.',
-'0':'-----', }
+'0':'-----', 'attention':'-.-.-'}
 
 
 with open(r'C:\Users\jorda\Desktop\Embedded I\Group-3\Morse_Code\MorseCode.txt') as file:#replace with pi path
     lines = [line for line in file.readlines()]
 
 translation = ''
-print (lines)
-for code in lines:
-   for char, morse in codes_dict.items():
-        if morse == code:
-            translation  += char
 
-#        print(translation)
 
-#file.close()
+for word in lines:
+    word = word.upper()
+    for letter in word:
+        code = str(codes_dict.get(letter))
+        if code == 'None':
+            translation += '\n '
+        else:
+            translation += code
+            translation += ' ' 
+
+       
+
+print (translation)
+        
+            
+
+
+file.close()
