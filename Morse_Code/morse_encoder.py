e6c0797c37b2ebb7d6e9d63ff0470753371ed3ca
jo0wi <jrw22710@uga.edu>
2024-03-19 14:03:21 -0400

    morse_encoder working

--- Morse_Code/morse_encoder.py
+++ Morse_Code/morse_encoder.py
@@ -2,39 +2,35 @@
 from time import sleep
 import numpy as np
 
-codes_dict = {'ATTENTION':'- . - . -', 'A':'. - ', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.',
-'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-',
-'L':'.-..', 'M':'--', 'N':'-.', 'O':'---', 'P':'.--.', 'Q':'--.-',
-'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--',
-'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--',
-'4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.',
-'0':'-----' }
+codes_dict = {'A':'. - ', 'B':'- . . .', 'C':'- . - .', 'D':'- . .', 'E':'.',
+'F':'. . - .', 'G':'- - . ', 'H':'. . . .', 'I':'. .', 'J':'. - - -', 'K':'- . -',
+'L':'. - . .', 'M':'- -', 'N':'- .', 'O':'- - -', 'P':'. - - .', 'Q':'- - . -',
+'R':'. - .', 'S':'. . .', 'T':'-', 'U':'. . -', 'V':'. . . -', 'W':'. - -',
+'X':'- . . -', 'Y':'- . - -', 'Z':'- - . .', '1':'. - - - -', '2':'. . - - -', '3':'. . . - -',
+'4':'. . . . -', '5':'. . . . .', '6':'- . . . .', '7':'- - . . .', '8':'- - - . .', '9':'- - - - .',
+'0':'- - - -  -' }
 
 
 with open(r'C:\Users\jorda\Desktop\Embedded I\Group-3\Morse_Code\MorseCode.txt') as file: #replace with pi path
     lines = [line for line in file.readlines()]
-
-translation = ''
+translation = '- . - . - | ATTENTION \n'
 file.close()
-
-for word in lines:
-    word = word.upper()
-    prosign = str(codes_dict.get(word))
-    if word == prosign:
-        translation += prosign
-        translation += ' '
-    else:
-        for letter in word:
-            code = str(codes_dict.get(letter))
-            if code == 'None':
-                translation += ' | ' + word + '\n'
-            else:
-                translation += code + ' '       
-
+for message in lines:
+    i = 0
+    message = message.upper()
+    word = message.split()
+    for letter in message:
+        code = str(codes_dict.get(letter))
+        if code == 'None':
+            translation += ' | ' + word[i] + '\n'
+            i += 1
+        else:
+            translation += code + '   ' 
+    translation += '- . - | OVER \n' 
 print (translation)
         
-f = open('encoded_message.txt', 'w')
-f.write(translation)
-f.close()
+#f = open('encoded_message.txt', 'w')
+#f.write(translation)
+#f.close()
 
 
