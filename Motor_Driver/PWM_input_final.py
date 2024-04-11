44d9b75780f86df0faa53389ba11cb6c84210919
jo0wi <jrw22710@uga.edu>
2024-04-11 13:33:14 -0400

    PWM and encoder combined

--- Motor_Driver/PWM_input_final.py
+++ Motor_Driver/PWM_input_final.py
@@ -1,10 +1,13 @@
-import RPi.GPIO as GPIO
+import RPI.GPIO as GPIO
 import time
 
 # Define GPIO pins for the encoder
 pin_a = 25
 pin_b = 24
 btn = 23
+pin_num = 21
+duty = 0
+
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -18,6 +21,13 @@ last_state_b = GPIO.input(pin_b)
 last_state_btn = GPIO.input(btn)
 
 debounce_delay = 0.001
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_num, GPIO.OUT, initial=GPIO.LOW)
+
+pwm = GPIO.PWM(pin_num, 10) #set frequency
+pwm.start(duty) #set duty cycle
 
 try:
     while True:
@@ -38,9 +48,11 @@ try:
         if state_a != last_state_a or state_b != last_state_b:
             if state_a != last_state_a:
                 if state_a != state_b:
+                    duty +=1
                     print('cw')
                 else:
                     print('ccw')
+                    duty -=1
         
         if btn_state != last_state_btn and btn_state == True:
             print('button pressed')
--- Motor_Driver/motor_driver.py
+++ Motor_Driver/motor_driver.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as RPI
 
 pin_num = 21
+duty = 0
 
 RPI.setwarnings(False)
 RPI.setmode(RPI.BCM)
 RPI.setup(pin_num, RPI.OUT, initial=RPI.LOW)
 
 pwm = RPI.PWM(pin_num, 10) #set frequency
-pwm.start(0) #set duty cycle
+pwm.start(duty) #set duty cycle
 
 while True:
   pass
